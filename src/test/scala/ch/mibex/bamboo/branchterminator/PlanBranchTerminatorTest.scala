5e3ebf006cab75c368ccb740d206489e9c66cd48
Michael Rüegg <rueegg.michael@gmail.com>
2015-11-16 11:51:55 +0100

    Refactored unit tests

--- src/test/scala/ch/mibex/bamboo/branchterminator/PlanBranchTerminatorTest.scala
+++ src/test/scala/ch/mibex/bamboo/branchterminator/PlanBranchTerminatorTest.scala
@@ -16,7 +16,7 @@ import scala.collection.JavaConverters._
 @RunWith(classOf[JUnitRunner])
 class PlanBranchTerminatorTest extends Specification with Mockito {
 
-  class AuthContext extends Scope {
+  class PlanBranchContext extends Scope {
     val deletionService = mock[DeletionService]
     val cachedPlanManager = mock[CachedPlanManager]
     val branchDeletionEvent = mock[StashBranchDeletedRemoteEvent]
@@ -25,40 +25,44 @@ class PlanBranchTerminatorTest extends Specification with Mockito {
 
   "plan branch terminator" should {
 
-    "not delete any plan branches when no matching ones were found" in new AuthContext {
-      cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query]) returns List[ImmutableChain]().asJava
+    "not delete any plan branches when no matching ones were found" in new PlanBranchContext {
+      (cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query])
+        returns List[ImmutableChain]().asJava)
 
       planBranchTerminator.onBranchChangedEvent(branchDeletionEvent)
 
       there was no(deletionService).deletePlans(any)
     }
 
-    "delete matching plan branch" in new AuthContext {
+    "delete matching plan branch" in new PlanBranchContext {
       val planBranch = mock[ImmutableChain]
       planBranch.getPlanKey returns PlanKeys.getPlanKey("TEST-TWOM22")
-      cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query]) returns List(planBranch).asJava
+      (cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query])
+        returns List(planBranch).asJava)
 
       planBranchTerminator.onBranchChangedEvent(branchDeletionEvent)
 
       there was one(deletionService).deletePlans(List("TEST-TWOM22").asJava)
     }
 
-    "not delete found plan branches not ending with a number in their key" in new AuthContext {
+    "not delete found plan branches not ending with a number in their key" in new PlanBranchContext {
       val planBranch = mock[ImmutableChain]
       planBranch.getPlanKey returns PlanKeys.getPlanKey("TEST-TWOM")
-      cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query]) returns List(planBranch).asJava
+      (cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query])
+        returns List(planBranch).asJava)
 
       planBranchTerminator.onBranchChangedEvent(branchDeletionEvent)
 
       there was no(deletionService).deletePlans(any)
     }
 
-    "delete all plan branches referencing deleted branch" in new AuthContext {
+    "delete all plan branches referencing deleted branch" in new PlanBranchContext {
       val planBranch1 = mock[ImmutableChain]
       planBranch1.getPlanKey returns PlanKeys.getPlanKey("TEST-ONEM11")
       val planBranch2 = mock[ImmutableChain]
       planBranch2.getPlanKey returns PlanKeys.getPlanKey("TEST-TWOM22")
-      cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query]) returns List(planBranch1, planBranch2).asJava
+      (cachedPlanManager.getPlansWithRepository(any[PlanRepositoryIndex.Query])
+        returns List(planBranch1, planBranch2).asJava)
 
       planBranchTerminator.onBranchChangedEvent(branchDeletionEvent)
 
